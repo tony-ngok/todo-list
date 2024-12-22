8febe8703fb3469a67ebc3059df0e02cc3ef244b
tony-ngok <tonyn1999@outlook.com>
2024-12-22 09:52:48 -0500

    表单、create 动作

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,14 @@ class TodosController < ApplicationController
   def index
     @todos = Todo.all
   end
+
+  def create
+    @todo = Todo.new(todo_params)
+    redirect_to @todo
+  end
+
+  private
+    def todo_params
+      params.require(:todo).permit(:name)
+    end
 end
