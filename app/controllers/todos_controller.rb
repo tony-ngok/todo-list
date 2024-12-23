221ae1f26d901a67284d1ae189d3939de6d08666
tony-ngok <tonyn1999@outlook.com>
2024-12-23 09:48:50 -0500

    任务列表：列表、创建

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,13 +3,22 @@ class TodosController < ApplicationController
     @todos = Todo.all
   end
 
+  def new
+    @todo = Todo.new
+  end
+
   def create
     @todo = Todo.new(todo_params)
-    redirect_to @todo
+
+    if @todo.save
+      redirect_to todos_path # 重定向至首页（若无，则没反应）
+    else
+      render "new", status: :unprocessable_entity
+    end
   end
 
   private
     def todo_params
-      params.require(:todo).permit(:name)
+      params.require(:todo).permit(:name, :important)
     end
 end
