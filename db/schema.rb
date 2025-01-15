ef88129d92e43d447889d01713da57f5bfe28c19
tony-ngok <tonyn1999@outlook.com>
2025-01-15 10:33:21 -0500

    初始化 devise、cancan

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_03_144631) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_15_152810) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_03_144631) do
     t.string "listname"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_listes_on_user_id"
   end
 
   create_table "todos", force: :cascade do |t|
@@ -27,7 +29,21 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_03_144631) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "liste_id"
+    t.bigint "user_id", null: false
     t.index ["liste_id"], name: "index_todos_on_liste_id"
+    t.index ["user_id"], name: "index_todos_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "todos", "listes"
